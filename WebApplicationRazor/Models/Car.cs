9359a6e0dcfb73dd367c9e84b56bc2d106116085
Vinicio Liuti <viniliuti@live.com>
2022-07-20 21:52:04 -0300

    Changing Car class so every field is required

--- WebApplicationRazor/Models/Car.cs
+++ WebApplicationRazor/Models/Car.cs
@@ -1,14 +1,23 @@
-﻿namespace WebApplicationRazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationRazor.Models
 {
     public class Car
     {
         public int Id { get; set; }
+        [Required]
         public string? Make { get; set; }
+        [Required]
         public string? Model { get; set; }
+        [Required]
         public int Year { get; set; }
+        [Required]
         public int Doors { get; set; }
+        [Required]
         public string? Color { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
 		public bool ShowPrice { get; set; }
     }
 }
